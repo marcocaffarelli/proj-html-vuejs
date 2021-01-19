47b9b4b21bd80a231e04055a6ae741cfd92b824f
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-19 14:46:43 +0100

    Completata la navbar
    Completata la navbar: aggiunto logo link e bottone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 let app = new Vue({
     el: '#app',
     data:{
-
+        //array di link
+        listLinkNavbar:[
+            'Home',
+            'About',
+            'Services',
+            'Blog',
+            'Contact',
+            'Portfolio',
+        ],
     },
 
     methods:{
