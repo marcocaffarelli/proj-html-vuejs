96082e05650d806ff795137c8013b25502b0819a
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-19 16:11:55 +0100

    Create le card della sezione servizi con vue
    Create le card della sezione servizi con vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,30 @@ let app = new Vue({
             'Contact',
             'Portfolio',
         ],
+
+        //card section servizi
+        cards:[
+            {
+                img:"group-247.png",
+                titolo:"Data Analysis",
+                testo:"When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+            },
+            {
+                img:"group-567.png",
+                titolo:"SEO Optimizazion",
+                testo:"When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+            },
+            {
+                img:"group-538.png",
+                titolo:"Security Data",
+                testo:"When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+            },
+            {
+                img:"group-566.png",
+                titolo:"Branding Strategy",
+                testo:"When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+            },
+        ],
     },
 
     methods:{
